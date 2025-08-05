75926b02b48f40009e3148dab700f891ab1eb84e
Dimitri Franco <147334685+francanico@users.noreply.github.com>
2025-08-05 15:47:39 -0400

    Update settings.py

--- gestor_seguros/gestor_seguros/settings.py
+++ gestor_seguros/gestor_seguros/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-%$9b3nwa0_vy9($qfp*37%mk2hg)a&w!6$23$#-hwna!xvrw9z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['gestorseguro-production.up.railway.app', 'localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://gestorseguro-production.up.railway.app']
 
 
 # Application definition
@@ -140,4 +142,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
