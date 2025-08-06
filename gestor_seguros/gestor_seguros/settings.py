53acdc01bfc09e6a934b638fa514f3a85481c155
Dimitri Franco <147334685+francanico@users.noreply.github.com>
2025-08-06 19:05:06 -0400

    Update settings.py

--- gestor_seguros/gestor_seguros/settings.py
+++ gestor_seguros/gestor_seguros/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-import sys
+import sys,dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv  
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,6 +142,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
