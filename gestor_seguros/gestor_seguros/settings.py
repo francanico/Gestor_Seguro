adbf8f4ef71c06782f5e88a4262cbb5d1ccd7e49
Dimitri Franco <dimitriFran07@gmail.com>
2025-10-29 23:14:07 -0400

    feat: Configura WhiteNoise para servir archivos estáticos en producción

--- gestor_seguros/gestor_seguros/settings.py
+++ gestor_seguros/gestor_seguros/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -135,7 +136,8 @@ LOGOUT_REDIRECT_URL = 'pagina_inicio' # A dónde ir después de cerrar sesión
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_production') # Para producción
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
